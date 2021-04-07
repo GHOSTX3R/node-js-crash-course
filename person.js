5a2aa9ef11e4cbf9cf61ae19ef298a2f44e89447
EL YAZIDI Mohamed <elyazidi.edu@gmail.com>
2021-04-07 20:34:14 +0100

    Initial commit

---
+++ person.js
@@ -0,0 +1,17 @@
+// Module Wrapper Function
+// (function (exports, require, module, __dirname, __filename) {});
+
+console.log(__dirname, __filename);
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greeting() {
+    console.log(`My name is ${this.name} and I am ${this.age} years old.`);
+  }
+}
+
+module.exports = Person;
---
+++ reference/url_demo.js
@@ -0,0 +1,33 @@
+const url = require('url');
+
+const myUrl = new URL(
+  'http://mywebsite.com:8000/hello.html?id=100&status=active'
+);
+
+// Serialized URL
+console.log(myUrl.href);
+console.log(myUrl.toString());
+
+// HOST (root domain) (does include port number)
+console.log(myUrl.host);
+
+// Hostname (doesnt include port number)
+console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialized query
+console.log(myUrl.search);
+
+// Params object
+console.log(myUrl.searchParams);
+
+// Add param
+myUrl.searchParams.append('abc', '123');
+console.log(myUrl.searchParams);
+
+// Loop through params
+myUrl.searchParams.forEach((value, name) => {
+  console.log(`${name}:${value}`);
+});
